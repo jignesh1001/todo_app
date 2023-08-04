144605c97ffe70589d5fd5a77c4528cb97f1268b
Jignesh <103873710+jignesh1001@users.noreply.github.com>
2023-08-04 18:14:32 +0530

    Update header.jsx

--- todos-list/src/components/header.jsx
+++ todos-list/src/components/header.jsx
@@ -7,7 +7,7 @@ function Header(props){
     return (
         <><nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand fs-2" href="#">{props.title}</a>
+          <a className="navbar-brand fs-2" href="#">TodoApp</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
